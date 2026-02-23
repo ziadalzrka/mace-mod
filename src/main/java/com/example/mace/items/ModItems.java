d242e9102f1ba9e3430a970ddba0b9e12a77c896
ziadalzrka <zeyadalzrka08@gmail.com>
2026-02-23 22:24:43 +0200

    Add ModItems class with new item definitions

---
+++ src/main/java/com/example/mace/items/ModItems.java
@@ -0,0 +1,7 @@
+package com.example.mace.items;
+import net.minecraft.item.Item;
+public class ModItems {
+    public static final Item MACE = new ItemMace();
+    public static final Item BREEZE_ROD = new Item().setRegistryName("breeze_rod").setUnlocalizedName("breeze_rod");
+    public static final Item HEAVY_CORE = new Item().setRegistryName("heavy_core").setUnlocalizedName("heavy_core");
+  }
