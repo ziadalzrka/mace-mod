ccb21d4ce06c33f1fa079e373eb8b5302312d74d
ziadalzrka <zeyadalzrka08@gmail.com>
2026-02-24 17:20:59 +0200

    Fix formatting and add newline at end of ModEnchantments.java

--- src/main/java/com/example/mace/enchantments/ModEnchantments.java
+++ src/main/java/com/example/mace/enchantments/ModEnchantments.java
@@ -3,7 +3,6 @@ package com.example.mace.enchantments;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnumEnchantmentType;
 import net.minecraft.inventory.EntityEquipmentSlot;
-
 public class ModEnchantments {
     public static final Enchantment DENSITY = new Enchantment(Enchantment.Rarity.COMMON, EnumEnchantmentType.WEAPON, new EntityEquipmentSlot[]{EntityEquipmentSlot.MAINHAND}){}.setRegistryName("density").setName("density");
     public static final Enchantment BREACH = new Enchantment(Enchantment.Rarity.RARE, EnumEnchantmentType.WEAPON, new EntityEquipmentSlot[]{EntityEquipmentSlot.MAINHAND}){}.setRegistryName("breach").setName("breach");
