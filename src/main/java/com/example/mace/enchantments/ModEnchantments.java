2b3d72df073a1f2c381df95283d289ace3917223
ziadalzrka <zeyadalzrka08@gmail.com>
2026-02-24 17:06:43 +0200

    Fix formatting in ModEnchantments.java

--- src/main/java/com/example/mace/enchantments/ModEnchantments.java
+++ src/main/java/com/example/mace/enchantments/ModEnchantments.java
@@ -8,4 +8,4 @@ public class ModEnchantments {
     public static final Enchantment DENSITY = new Enchantment(Enchantment.Rarity.COMMON, EnumEnchantmentType.WEAPON, new EntityEquipmentSlot[]{EntityEquipmentSlot.MAINHAND}){}.setRegistryName("density").setName("density");
     public static final Enchantment BREACH = new Enchantment(Enchantment.Rarity.RARE, EnumEnchantmentType.WEAPON, new EntityEquipmentSlot[]{EntityEquipmentSlot.MAINHAND}){}.setRegistryName("breach").setName("breach");
     public static final Enchantment WIND_BURST = new Enchantment(Enchantment.Rarity.VERY_RARE, EnumEnchantmentType.WEAPON, new EntityEquipmentSlot[]{EntityEquipmentSlot.MAINHAND}){}.setRegistryName("wind_burst").setName("wind_burst");
-                                                                                                                                               }
+                                                             }
